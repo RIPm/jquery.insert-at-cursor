95a08b90d35896317219b0dee603b39a523e3fad
R.I.P <371662584@qq.com>
2017-11-10 18:16:14 +0800

    finish function

---
+++ README.md
@@ -0,0 +1,35 @@
+# jquery.insert-at-cursor
+
+> Jquery plugin for insert anything at the cursor position
+
+## Usage
+
+```html
+<script src="jquery.insert-at-cursor.min.js"></script>
+```
+
+#### html
+
+``` html
+<input type="text" value="" id="foo" />
+```
+or
+
+``` html
+<textarea id="foo"></textarea>
+```
+or
+``` html
+<div contenteditable="true" id="foo"></div>
+```
+
+#### javascript
+```js
+// Normal string
+$("#foo").insertAtCursor('bar');
+
+// At contenteditable dom
+$("#foo").insertAtCursor('<span>bar</span>');
+// At contenteditable dom (functions like rich text editors)
+$("#foo").insertAtCursor('<span contenteditable="false">bar</span>');
+```
---
+++ jquery.insert-at-cursor.js
@@ -0,0 +1,106 @@
+(function ($, document, window, undefined) {
+    var DATANAME = 'shadonghongCaret';
+
+    var saveSelection =  function() {
+        if(window.getSelection) {
+            /*主流的浏览器，包括chrome、Mozilla、Safari*/
+            var sel = window.getSelection();
+            if(sel.rangeCount > 0) {
+                return sel.getRangeAt(0);
+            }
+        } else if(document.selection) {
+            /*IE下的处理*/
+            return document.selection.createRange();
+        }
+        return null;
+    }
+
+    var initCursor = function(dom) {
+        $(dom).off('mouseup keyup').on('mouseup keyup', function() {
+            //selectedRange = saveSelection();
+            $(this).data(DATANAME, saveSelection());
+        });
+        $(dom).each(function() {
+            if(!$(this).hasfocus) {
+                $(this).focus();
+                var range = saveSelection();
+                range.selectNodeContents(this);
+                range.collapse(false);
+                $(this).data(DATANAME, range);
+                $(this).blur();
+            }
+        });
+    };
+
+    $.initCursor = initCursor;
+
+    $.fn.insertAtCursor = function (text) {
+        return this.each(function () {
+            var input = this, scrollPos, strPosStart = 0, strPosEnd = 0, isModernBrowser = ("selectionStart" in input && "selectionEnd" in input), before, after, range, selection, node;
+
+            if (!((input.tagName && input.tagName.toLowerCase() === "textarea") || (input.tagName && input.tagName.toLowerCase() === "input" && input.type.toLowerCase() === "text"))) {
+
+                if(!$(input).hasfocus) {
+                    $(input).focus();
+                }
+
+                if (window.getSelection && window.getSelection().getRangeAt) {
+                    range = $(input).data(DATANAME) || (
+                        initCursor(input),
+                        $(input).data(DATANAME)
+                    );
+                    range.collapse(false);
+                    node = range.createContextualFragment(text);
+
+                    var c = node.lastChild;
+
+                    range.insertNode(node);
+
+                    if(c){
+                        range.setEndAfter(c);
+                        range.setStartAfter(c);
+                    }
+
+                    var j = window.getSelection();
+                    j.removeAllRanges();
+                    j.addRange(range);
+
+                } else if (document.selection && document.selection.createRange) {
+                    document.selection.createRange().pasteHTML(text);
+                }
+            }else {
+                scrollPos = input.scrollTop;
+
+                if (isModernBrowser) {
+                    strPosStart = input.selectionStart;
+                    strPosEnd = input.selectionEnd;
+                } else {
+                    input.focus();
+                    range = document.selection.createRange();
+                    range.moveStart('character', -input.value.length);
+                    strPosStart = range.text.length;
+                }
+
+                if (strPosEnd < strPosStart)
+                    strPosEnd = strPosStart;
+
+                before = (input.value).substring(0, strPosStart);
+                after = (input.value).substring(strPosEnd, input.value.length);
+                input.value = before + text + after;
+                strPosStart = strPosStart + text.length;
+
+                if (isModernBrowser) {
+                    input.selectionStart = strPosStart;
+                    input.selectionEnd = strPosStart;
+                } else {
+                    range = document.selection.createRange();
+                    range.moveStart('character', strPosStart);
+                    range.moveEnd('character', 0);
+                    range.select();
+                }
+
+                input.scrollTop = scrollPos;
+            }
+        });
+    };
+})(jQuery, document, window);
